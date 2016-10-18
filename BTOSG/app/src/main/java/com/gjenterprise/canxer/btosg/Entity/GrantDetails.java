fecd4880e9510a318030015920e30ab1b19209b2
Thomas Lim <thomaslim93@gmail.com>
2016-10-18 16:50:09 +0800

    Update on GrantDetails & Input

--- BTOSG/app/src/main/java/com/gjenterprise/canxer/btosg/Entity/GrantDetails.java
+++ BTOSG/app/src/main/java/com/gjenterprise/canxer/btosg/Entity/GrantDetails.java
@@ -4,4 +4,31 @@ package com.gjenterprise.canxer.btosg.Entity;
  * Created by Canxer on 8/10/2016.
  */
 public class GrantDetails {
+
+    private String grantType;
+
+    private double grantAmount;
+
+
+    public GrantDetails(String grantType, double grantAmount){
+        this.grantType = grantType;
+        this.grantAmount = grantAmount;
+    }
+
+    public void setGrantType(String grantType){
+        this.grantType = grantType;
+    }
+
+    public void setGrantAmount(double grantAmount){
+        this.grantAmount = grantAmount;
+    }
+
+    public String getGetGrantType(){
+        return grantType;
+    }
+
+    public double getGrantAmount(){
+        return grantAmount;
+    }
+
 }
--- BTOSG/app/src/main/java/com/gjenterprise/canxer/btosg/Entity/GrantInput.java
+++ BTOSG/app/src/main/java/com/gjenterprise/canxer/btosg/Entity/GrantInput.java
@@ -3,5 +3,44 @@ package com.gjenterprise.canxer.btosg.Entity;
 /**
  * Created by Canxer on 8/10/2016.
  */
+
+import java.util.Date;
 public class GrantInput {
+
+    private String typeOfApplicant;
+
+    private double householdIncome;
+
+    private Date salesPeriod;
+
+    public GrantInput(String typeOfApplicant, double householdIncome, Date salesPeriod){
+        this.typeOfApplicant = typeOfApplicant;
+        this.householdIncome = householdIncome;
+        this.salesPeriod = salesPeriod;
+    }
+
+    public Date getSalesPeriod() {
+        return salesPeriod;
+    }
+
+    public double getHouseholdIncome(){
+        return householdIncome;
+    }
+
+    public String getTypeOfApplicant(){
+        return typeOfApplicant;
+    }
+
+    public void setTypeOfApplicant(String typeOfApplicant){
+        this.typeOfApplicant = typeOfApplicant;
+    }
+
+    public void setHouseholdIncome(double householdIncome){
+        this.householdIncome = householdIncome;
+    }
+
+    public void setSalesPeriod(Date salesPeriod){
+        this.salesPeriod = salesPeriod;
+    }
 }
+
